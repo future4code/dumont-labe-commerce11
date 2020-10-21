c8171c2358f76975b71939f93f8b7f0c94d60788
Kelly Knoblauch <kellyknoblauch@MacBook-Pro-de-Kelly.local>
2020-10-21 19:19:13 -0300

    corrigindo as consequencias dos meus atos

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <p>HELLO WORLD!</p>
-        <Filter />
-        <Products />
-        <ShoppingCart />
-        <button></button>
+        <p>hello</p>
       </div>
     )
   }

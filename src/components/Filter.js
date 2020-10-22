b41b8f3bc881e0dbcbc8c0e5e808c00cf2dfa626
Marina Azzi Nogueira <azzinogueira.marina@google.com>
2020-10-22 12:37:33 -0300

    estrutura do filtro pronta, adicionei o ShoppingCartItem.js da kelly

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,13 +3,39 @@ import styled from 'styled-components';
 
 const FilterContainer = styled.div`
     border: 1px solid black;
+    padding: 8px;
+`
+
+const InputContainer = styled.label`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-bottom: 8px;
 `
 
 class Filter extends React.Component {
     render() {
         return (
             <FilterContainer>
-                ESSE É O FILTRO
+                <h3>Filtros</h3>
+                <div>
+                    <label>
+                        Valor Mínimo:
+                        <input type='number' />
+                </label>
+                </div>
+                <div>
+                    <label>
+                        Valor Máximo:
+                        <input type='number' />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Buscar Produto:
+                        <input type='text' />
+                </label>
+                </div>
             </FilterContainer>
         )
     }
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,22 +1,39 @@
 import React from "react";
+import styled from 'styled-components';
+const CardContainer = styled.div`
+    border: 1px solid black;
+`
+const CardInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    margin: 4px 0;
+`
 
-
-
+const AddButton = styled.button`
+    align-self: center;
+    margin-top: 4px;
+`
 
 class ProductCard extends React.Component {
     render(){
         const product = this.props.product
         return(
-            <div>
-                <img src={product.image}/>
+            <CardContainer>
+                {/* <img src={product.image}/>
+                <CardInfo>
                 <p>{product.item}</p>
-                <p>R${product.price}</p>
-                <button>Adicionar ao carrinho</button>
-            </div>
-            
+                <p>{product.price}</p>
+                <AddButton>Adicionar ao carrinho</AddButton>
+                </CardInfo> */}
+                <img src='https://picsum.photos/200/200?a=1'/>
+                <CardInfo>
+                <p>item: 'Item 1'</p>
+                <p>price: 100,</p>
+                <AddButton>Adicionar ao carrinho</AddButton>
+                </CardInfo>
+            </CardContainer>
         )
     }
-
 }
-
 export default ProductCard

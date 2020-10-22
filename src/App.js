640549dadf48754c5087e52ce2b28ecfa753b83f
Marina Azzi Nogueira <azzinogueira.marina@google.com>
2020-10-22 18:34:58 -0300

    versão estática: filtros funcionando

--- src/App.js
+++ src/App.js
@@ -22,54 +22,107 @@ const products = [
     id: 2,
     image: 'https://picsum.photos/200/200?a=2',
     name: 'Produto 2',
-    price: 100
+    price: 200
   },
   {
     id: 3,
     image: 'https://picsum.photos/200/200?a=3',
     name: 'Produto 3',
-    price: 100
+    price: 300
   },
   {
     id: 4,
     image: 'https://picsum.photos/200/200?a=4',
     name: 'Produto 4',
-    price: 100
+    price: 400
   },
   {
     id: 5,
     image: 'https://picsum.photos/200/200?a=5',
     name: 'Produto 5',
-    price: 100
+    price: 500
   },
   {
     id: 6,
     image: 'https://picsum.photos/200/200?a=6',
     name: 'Produto 6',
-    price: 100
+    price: 600
   },
   {
     id: 7,
     image: 'https://picsum.photos/200/200?a=7',
     name: 'Produto 7',
-    price: 100
+    price: 700
   },
   {
     id: 8,
     image: 'https://picsum.photos/200/200?a=8',
     name: 'Produto 8',
-    price: 100
+    price: 800
   },
 ]
 
-function App() {
+class App extends React.Component {
+  state = {
+    minFilter: '',
+    maxFilter: '',
+    nameFilter: '',
+    productsList: products
+  }
+
+  componentDidMount() {
+    this.getFilteredAndOrderedList()
+  }
+
+  getFilteredAndOrderedList = () => {
+    const filteredList = products
+        .filter((product) => {
+          if (this.state.maxFilter) {
+            return(product.price <= Number(this.state.maxFilter))
+          } else {
+            return true
+          }
+        })
+
+        .filter((product) => {
+          if (this.state.minFilter) {
+            return(product.price >= Number(this.state.minFilter))
+          } else {
+            return true
+          } 
+        })
+
+        .filter((product) => {
+          if (this.state.nameFilter) {
+            return(this.state.nameFilter && product.name.includes(this.state.nameFilter))
+          } else {
+            return true
+          }
+        })
+              
+    this.setState({ productsList: filteredList })
+    
+}
+
+  render () {
     return (
       <AppContainer>
-        <Filter />
-        <Products products={products} />
+        <Filter
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+        />
+        <Products 
+          products={this.state.productsList}
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter} 
+        />
         <ShoppingCart />
       </AppContainer>
     )
+  }  
+  
   
 }
 

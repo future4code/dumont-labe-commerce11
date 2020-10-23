32eddd6dc3b016f233ef8586e72fb4cdc83b4712
Kelly Knoblauch <kellyknoblauch@MacBook-Pro-de-Kelly.local>
2020-10-23 11:48:44 -0300

    fazendo todos estaticos, menos ordenacao

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,26 +1,23 @@
 import React from "react";
 import styled from 'styled-components';
 
+
 const CardContainer = styled.div`
     border: 1px solid black;
-
+    display: flex;
+    flex-direction: column;
 `
-
 const CardInfo = styled.div`
     display: flex;
     flex-direction: column;
-    padding: 16px;
-
     p{
-    margin: 4px 0
+        margin: 4px 0
 }
 `;
-
 const AddButton = styled.button`
     align-self: center;
     margin-top: 4px;
 `
-
 class ProductCard extends React.Component {
     render(){
         const product = this.props.product
@@ -28,15 +25,12 @@ class ProductCard extends React.Component {
             <CardContainer>
                 <img src={product.image}/>
                 <CardInfo>
-                    <p>{product.name}</p>
+                    <p><b>{product.name}</b></p>
                     <p>R${product.price},00</p>
                     <AddButton>Adicionar ao carrinho</AddButton>
                 </CardInfo>
             </CardContainer>
-            
         )
     }
-
 }
-
 export default ProductCard

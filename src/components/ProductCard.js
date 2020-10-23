9669db2305c935f535b43b7333a354250f458666
Marina Azzi Nogueira <72823067+azzinogueira-marina@users.noreply.github.com>
2020-10-23 12:20:58 -0300

    Revert "fazendo todos estaticos, menos ordenacao"

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,23 +1,26 @@
 import React from "react";
 import styled from 'styled-components';
 
-
 const CardContainer = styled.div`
     border: 1px solid black;
-    display: flex;
-    flex-direction: column;
+
 `
+
 const CardInfo = styled.div`
     display: flex;
     flex-direction: column;
+    padding: 16px;
+
     p{
-        margin: 4px 0
+    margin: 4px 0
 }
 `;
+
 const AddButton = styled.button`
     align-self: center;
     margin-top: 4px;
 `
+
 class ProductCard extends React.Component {
     render(){
         const product = this.props.product
@@ -25,12 +28,15 @@ class ProductCard extends React.Component {
             <CardContainer>
                 <img src={product.image}/>
                 <CardInfo>
-                    <p><b>{product.name}</b></p>
+                    <p>{product.name}</p>
                     <p>R${product.price},00</p>
                     <AddButton>Adicionar ao carrinho</AddButton>
                 </CardInfo>
             </CardContainer>
+            
         )
     }
+
 }
+
 export default ProductCard
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-import ShoppingCartItem from './ShoppingCartItem'
-
-
+import { ShoppingItem } from './ShoppingItem';
 
 const ShoppingCartContainer = styled.div`
     border: 1px solid black;
 `;
+
 const CartlistContainer = styled.div`
-    display: grid;
+    display: grid; 
     gap: 4px;
+
 `
+
+
 class ShoppingCart extends React.Component {
-    getTotalValue = () => {
-        let totalValue = 0
-        for (let product of this.props.productsInCart){
-            totalValue += product.price * product.quantity
-        }
-        return totalValue
-    }
     render() {
         return (
             <ShoppingCartContainer>
                 <h3>Carrinho: </h3>
-                <CartlistContainer>
-                    {this.props.productsInCart.map((product) => {
-                        return <ShoppingCartItem cartItem = {product}/>
-                    })}
-                </CartlistContainer>
-                <p>Valor total: R${this.getTotalValue()},00</p>
+                <ShoppingItem/>
             </ShoppingCartContainer>
         )
     }
 }
+
 export default ShoppingCart
